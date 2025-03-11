756df3c0d3813475474076f5b20b29e0fee924d9
yunweneric <yunweneric@gmail.com>
2025-03-11 23:47:47 +0100

    adding app ui

--- apps/tribu/src/app/app.tsx
+++ apps/tribu/src/app/app.tsx
@@ -3,7 +3,7 @@ import { Home } from '@tribu/home';
 import { SurveyHome, SurveyTemplates } from '@tribu/surveys';
 import { AudienceHome, NewAudienceGroup } from '@tribu/audience';
 import FormBuilder from './form_builder_screen';
-import { Outlet, Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 export function App() {
   const navigate = useNavigate();
