64cfe0497f757f77602f60b2332f915888073d6f
yunweneric <yunweneric@gmail.com>
2025-04-15 09:58:40 +0100

    feat: Add maps library with Google Maps integration and related configurations

---
+++ libs/maps/src/lib/audience_map.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { AppInput } from '@tribu/forms';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const AudienceMap = () => {
+  const containerStyle = {
+    width: '400px',
+    height: '400px',
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'YOUR_API_KEY',
+  });
+
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
+    setMap(null);
+  }, []);
+
+  return (
+    <div>
+      <AppInput type="text" id="" onChange={(e) => {}} />
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      )}
+      : (<></>)
+    </div>
+  );
+};
+
+export default AudienceMap;
